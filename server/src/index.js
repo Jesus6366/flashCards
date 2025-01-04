9e0449a8c80896003c053f530bd8c8f2d12c8c3c
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-04 09:03:38 -0700

    server and tyscript configuration added

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
